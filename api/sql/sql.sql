e317466ddd405fda18a64ecacd4f70684ebf2a8e
devmarcosav <devmarcosav@gmail.com>
2023-10-11 18:27:55 -0300

    feat: buscar, buscar seg, atualizar senha, models publicacoes, rotas publicacoes

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE IF NOT EXISTS devbook1;
 USE devbook1;
 
-DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS publicacoes;
 DROP TABLE IF EXISTS seguidores;
+DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios(
   id int auto_increment primary key,
@@ -26,3 +27,17 @@ CREATE TABLE seguidores(
 
   primary key (usuario_id, seguidor_id)
 )ENGINE=INNODB;
+
+CREATE TABLE publicacoes(
+  id int auto_increment primary key,
+  titulo varchar(50) not null,
+  conteudo varchar(300) not null,
+
+  autor_id int not null,
+  FOREIGN KEY (autor_id) 
+  REFERENCES usuarios(id)
+  on DELETE CASCADE,
+
+  curtidas int default 0,
+  criadoEm timestamp default current_timestamp()
+) ENGINE=INNODB; 
---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "net/http"
+
+//*CriarPublicao permite o usuario criar uma publicacao
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//*BuscarPublicacoes permite o usuario buscar todas publicacoes no feed do usuario
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request)   {
+
+}
+
+//*BuscarPublicacao permite o usuario buscar uma publicacao
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request)    {
+
+}
+
+//*AtualizarPublicacao permite o usuario atualizar uma determinada publicacao
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+//*DeletarPublicacao permite o usuario deletar uma publicacao
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request)   {
+
+}
